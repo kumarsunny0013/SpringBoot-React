cf024bfadec22c4d578e607b7b4bea64804d21d1
kumarsunny0013 <kumarsunny0013@gmail.com>
2022-08-06 10:58:40 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Ws from './ws';
+
+function App() {
+  return (
+    <div className="App">
+     <h2 >Testing WebSocket</h2>
+      <Ws/>
+    </div>
+  );
+}
+
+export default App;
